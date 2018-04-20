bf0f701a2a51b37554d775463f0cfa8edbeab76b
Ashvin Nair <nair.ashvin@gmail.com>
2018-04-19 20:46:16 -0700

    Adding pycuda and working docker file

---
+++ docker/vendor/pycuda/pycuda/cumath.py
@@ -0,0 +1,135 @@
+from __future__ import absolute_import
+import pycuda.gpuarray as gpuarray
+import pycuda.elementwise as elementwise
+import numpy as np
+import warnings
+from pycuda.driver import Stream
+
+
+def _make_unary_array_func(name):
+    def f(array, stream_or_out=None, **kwargs):
+
+        if stream_or_out is not None:
+            warnings.warn("please use 'out' or 'stream' keyword arguments", DeprecationWarning)
+            if isinstance(stream_or_out, Stream):
+                stream = stream_or_out
+                out = None
+            else:
+                stream = None
+                out = stream_or_out
+
+        out, stream = None, None
+        if 'out' in kwargs:
+            out = kwargs['out']
+        if 'stream' in kwargs:
+            stream = kwargs['stream']
+
+        if array.dtype == np.float32:
+            func_name = name + "f"
+        else:
+            func_name = name
+
+        if not array.flags.forc:
+            raise RuntimeError("only contiguous arrays may "
+                    "be used as arguments to this operation")
+
+        if out is None:
+            out = array._new_like_me()
+        else:
+            assert out.dtype == array.dtype
+            assert out.strides == array.strides
+            assert out.shape == array.shape
+
+        func = elementwise.get_unary_func_kernel(func_name, array.dtype)
+        func.prepared_async_call(array._grid, array._block, stream,
+                array.gpudata, out.gpudata, array.mem_size)
+
+        return out
+    return f
+
+fabs = _make_unary_array_func("fabs")
+ceil = _make_unary_array_func("ceil")
+floor = _make_unary_array_func("floor")
+exp = _make_unary_array_func("exp")
+log = _make_unary_array_func("log")
+log10 = _make_unary_array_func("log10")
+sqrt = _make_unary_array_func("sqrt")
+
+sin = _make_unary_array_func("sin")
+cos = _make_unary_array_func("cos")
+tan = _make_unary_array_func("tan")
+asin = _make_unary_array_func("asin")
+acos = _make_unary_array_func("acos")
+atan = _make_unary_array_func("atan")
+
+sinh = _make_unary_array_func("sinh")
+cosh = _make_unary_array_func("cosh")
+tanh = _make_unary_array_func("tanh")
+
+def fmod(arg, mod, stream=None):
+    """Return the floating point remainder of the division `arg/mod`,
+    for each element in `arg` and `mod`."""
+    result = gpuarray.GPUArray(arg.shape, arg.dtype)
+
+    if not arg.flags.forc or not mod.flags.forc:
+        raise RuntimeError("only contiguous arrays may "
+                "be used as arguments to this operation")
+
+    func = elementwise.get_fmod_kernel()
+    func.prepared_async_call(arg._grid, arg._block, stream,
+            arg.gpudata, mod.gpudata, result.gpudata, arg.mem_size)
+
+    return result
+
+def frexp(arg, stream=None):
+    """Return a tuple `(significands, exponents)` such that
+    `arg == significand * 2**exponent`.
+    """
+    if not arg.flags.forc:
+        raise RuntimeError("only contiguous arrays may "
+                "be used as arguments to this operation")
+
+    sig = gpuarray.GPUArray(arg.shape, arg.dtype)
+    expt = gpuarray.GPUArray(arg.shape, arg.dtype)
+
+    func = elementwise.get_frexp_kernel()
+    func.prepared_async_call(arg._grid, arg._block, stream,
+            arg.gpudata, sig.gpudata, expt.gpudata, arg.mem_size)
+
+    return sig, expt
+
+def ldexp(significand, exponent, stream=None):
+    """Return a new array of floating point values composed from the
+    entries of `significand` and `exponent`, paired together as
+    `result = significand * 2**exponent`.
+    """
+    if not significand.flags.forc or not exponent.flags.forc:
+        raise RuntimeError("only contiguous arrays may "
+                "be used as arguments to this operation")
+
+    result = gpuarray.GPUArray(significand.shape, significand.dtype)
+
+    func = elementwise.get_ldexp_kernel()
+    func.prepared_async_call(significand._grid, significand._block, stream,
+            significand.gpudata, exponent.gpudata, result.gpudata,
+            significand.mem_size)
+
+    return result
+
+def modf(arg, stream=None):
+    """Return a tuple `(fracpart, intpart)` of arrays containing the
+    integer and fractional parts of `arg`.
+    """
+    if not arg.flags.forc:
+        raise RuntimeError("only contiguous arrays may "
+                "be used as arguments to this operation")
+
+    intpart = gpuarray.GPUArray(arg.shape, arg.dtype)
+    fracpart = gpuarray.GPUArray(arg.shape, arg.dtype)
+
+    func = elementwise.get_modf_kernel()
+    func.prepared_async_call(arg._grid, arg._block, stream,
+            arg.gpudata, intpart.gpudata, fracpart.gpudata,
+            arg.mem_size)
+
+    return fracpart, intpart
---
+++ docker/vendor/pycuda/test/undistributed/measure_gpuarray_speed.py
@@ -0,0 +1,89 @@
+from __future__ import absolute_import
+from __future__ import print_function
+#! /usr/bin/env python
+import pycuda.driver as drv
+import pycuda.autoinit
+import numpy
+import numpy.linalg as la
+from six.moves import range
+from six.moves import zip
+
+
+
+
+def main():
+    import pycuda.gpuarray as gpuarray
+
+    sizes = []
+    times_gpu = []
+    flops_gpu = []
+    flops_cpu = []
+    times_cpu = []
+
+    from pycuda.tools import bitlog2
+    max_power = bitlog2(drv.mem_get_info()[0]) - 2
+    # they're floats, i.e. 4 bytes each
+    for power in range(10, max_power):
+        size = 1<<power
+        print(size)
+        sizes.append(size)
+        a = gpuarray.zeros((size,), dtype=numpy.float32)
+        b = gpuarray.zeros((size,), dtype=numpy.float32)
+        b.fill(1)
+
+        if power > 20:
+            count = 100
+        else:
+            count = 1000
+
+        # gpu -----------------------------------------------------------------
+        start = drv.Event()
+        end = drv.Event()
+        start.record()
+
+        for i in range(count):
+            a+b
+
+        end.record()
+        end.synchronize()
+
+        secs = start.time_till(end)*1e-3
+
+        times_gpu.append(secs/count)
+        flops_gpu.append(size)
+        del a
+        del b
+
+        # cpu -----------------------------------------------------------------
+        a_cpu = numpy.random.randn(size).astype(numpy.float32)
+        b_cpu = numpy.random.randn(size).astype(numpy.float32)
+
+        #start timer
+        from time import time
+        start = time()
+        for i in range(count):
+            a_cpu + b_cpu
+        secs = time() - start
+
+        times_cpu.append(secs/count)
+        flops_cpu.append(size)
+
+
+    # calculate pseudo flops
+    flops_gpu = [f/t for f, t in zip(flops_gpu,times_gpu)]
+    flops_cpu = [f/t for f, t in zip(flops_cpu,times_cpu)]
+
+    from pytools import Table
+    tbl = Table()
+    tbl.add_row(("Size", "Time GPU", "Size/Time GPU",
+        "Time CPU","Size/Time CPU","GPU vs CPU speedup"))
+    for s, t, f, t_cpu, f_cpu in zip(sizes, times_gpu, flops_gpu, times_cpu, flops_cpu):
+        tbl.add_row((s, t, f, t_cpu, f_cpu, f/f_cpu))
+    print(tbl)
+
+
+
+
+
+if __name__ == "__main__":
+    main()
